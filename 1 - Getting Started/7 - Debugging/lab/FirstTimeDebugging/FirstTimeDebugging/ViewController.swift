73e85f1cbdb3aad7bdbc28aa81d751451b2564b6
Franco Cedillo <franco.cedillo@gmail.com>
2017-06-05 15:53:18 -0500

    initial commit, Strings

---
+++ 1 - Getting Started/7 - Debugging/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let sample = "sample"
+        print(sample)
+        
+        
+    }
+    
+    func someMethod() {
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+/*:
+ ## Exercise - String Basics
+ 
+ Create a `name` constant and assign it a string literal representing your name.
+ */
+let name = "Framco"
+
+/*:
+ Create a `favoriteQuote` constant and assign it the following string literal:
+ 
+ - "My favorite quote is <INSERT QUOTE HERE>."
+ 
+ Write in your own favorite quote where indicated, and be sure to include escaped quotation marks. Print the value of `favoriteQuote`
+ */
+let favoriteQuote = "My favorite quote is \"Como las flores que al ser cortadas brindan su perfume\""
+
+/*:
+ Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
+ */
+let emptyString = ""
+
+if emptyString.isEmpty {
+    print("There's nothing here")
+} else {
+    print("It's not as empty as I thought")
+}
+
+//: page 1 of 5  |  [Next: Exercise - Concatenation and Interpolation](@next)
