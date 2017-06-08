e844537a0f8840eb5e224d2a02f724d6b6b5b25c
Franco Cedillo <franco.cedillo@gmail.com>
2017-06-07 21:23:37 -0500

    2.4 Classes, Inheritance

--- 2 - Introduction to UIKit/4 - Classes and Inheritance/lab/Lab - Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/4 - Classes and Inheritance/lab/Lab - Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
@@ -23,21 +23,51 @@ class Spaceship {
 /*:
  Define a new class `Fighter` that inherits from `Spaceship`. Add a variable property `weapon` that defaults to an empty string and a variable property `remainingFirePower` that defaults to 5.
  */
-
+class Fighter: Spaceship {
+    
+    var weapon: String = ""
+    var remainingFirePower = 5
+    
+    func fire() {
+        if remainingFirePower > 0 {
+            remainingFirePower -= 1
+        } else {
+            print("You have no more fire power")
+        }
+    }
+    
+}
 
 /*:
  Create a new instance of `Fighter` called `destroyer`. A `Fighter` will be able to shoot incoming objects to avoid colliding with them. After initialization, set `weapon` to "Laser" and `remainingFirePower` to 10. Note that since `Fighter` inherits from `Spaceship`, it also has properties for `name`, `health`, and `position`, and has methods for `moveLeft()`, `moveRight()`, and `wasHit()` even though you did not specifically add them to the declaration of `Fighter`. Knowing that, set `name` to "Destroyer," print `position`, then call `moveRight()` and print `position` again.
  */
-
-
+var destroyer = Fighter()
+destroyer.weapon = "Laser"
+destroyer.remainingFirePower = 10
+destroyer.name = "Destroyer"
+destroyer.position
+destroyer.moveRight()
+destroyer.position
 /*:
  Try to print `weapon` on `falcon`. Why doesn't this work? Provide your answer in a comment or a print statement below, and remove any code you added that doesn't compile.
  */
-
-
+ //falcon.weapon
+ // because weapon is not a property of Spaceship
 /*:
  Add a method to `fighter` called `fire()`. This should check to see if `remainingFirePower` is greater than 0, and if so, should decrement `remainingFirePower` by one. If `remainingFirePower` is not greater than 0, print "You have no more fire power." Call `fire()` on `destroyer` a few times and print `remainingFirePower` after each method call.
  */
-
-
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+destroyer.remainingFirePower
+destroyer.fire()
+destroyer.remainingFirePower
+destroyer.fire()
+destroyer.fire()
 //: [Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Override Methods and Properties](@next)
--- 2 - Introduction to UIKit/4 - Classes and Inheritance/lab/Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/4 - Classes and Inheritance/lab/Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
@@ -19,6 +19,12 @@ class Spaceship {
     func wasHit() {
         health -= 5
     }
+    
+    init(name: String, health: Int, position: Int) {
+        self.name = name
+        self.health = health
+        self.position = position
+    }
 }
 
 class Fighter: Spaceship {
@@ -32,6 +38,13 @@ class Fighter: Spaceship {
             print("You have no more fire power.")
         }
     }
+    
+    init(weapon: String, remainingFirePower: Int, name: String, health: Int, position: Int) {
+        self.weapon = weapon
+        self.remainingFirePower = remainingFirePower
+        super.init(name: name, health: health, position: position)
+    }
+    
 }
 
 class ShieldedShip: Fighter {
@@ -44,33 +57,44 @@ class ShieldedShip: Fighter {
             super.wasHit()
         }
     }
+    
+    init(shieldStrength: Int, weapon: String, remainingFirePower: Int, name: String, health: Int, position: Int) {
+        self.shieldStrength = shieldStrength
+        super.init(weapon: weapon, remainingFirePower: remainingFirePower, name: name, health: health, position: position)
+    }
 }
 /*:
  Note that each class above has an error by the class declaration that says "Class has no initializers." Unlike structs, classes do not come with memberwise initializers because the standard memberwise initializers don't always play nicely with inheritance. You can get rid of the error by providing default values for everything, but it is common and better practice to simply write your own initializer. Go to the declaration of `Spaceship` and add an initializer that takes in an argument for each property on `Spaceship` and sets the properties accordingly. 
  
  Then create an instance of `Spaceship` below called `falcon`. Use the memberwise initializer you just created. The ship's name should be "Falcon."
  */
-
+var falcon = Spaceship(name: "Falcon", health: 10, position: 0)
 
 /*:
  Writing initializers for subclasses can get tricky. Your initializer needs to not only set the properties declared on the subclass, but also set all of the uninitialized properties on classes that it inherits from. Go to the declaration of `Fighter` and write an initializer that takes an argument for each property on `Fighter` and for each property on `Spaceship`. Set the properties accordingly. (Hint: you can call through to a superclass' initializer with `super.init` *after* you initialize all of the properties on the subclass).
  
  Then create an instance of `Fighter` below called `destroyer`. Use the memberwise initializer you just created. The ship's name should be "Destroyer."
  */
-
+var destroyer = Fighter(weapon: "Disparon", remainingFirePower: 10, name: "Destroyer", health: 25, position: 1)
 
 /*:
  Now go add an initializer to `ShieldedShip` that takes an argument for each property on `ShieldedShip`, `Fighter`, and `Spaceship`, and sets the properties accordingly. Remember that you can call through to the initializer on `Fighter` using `super.init`.
  
  Then create an instance of `ShieldedShip` below called `defender`. Use the memberwise initializer you just created. The ship's name should be "Defender."
  */
-
+var defender = ShieldedShip(shieldStrength: 25, weapon: "Cannon", remainingFirePower: 10, name: "Defender", health: 10, position: 1)
 
 /*:
  Create a new instance of `Spaceship` called `sameShip` and set it equal to `falcon`. Print out the position of `sameShip` and `falcon`, then call `moveLeft()` on `sameShip` and print out the position of `sameShip` and `falcon` again. Did both positions change? Why? If both were structs instead of classes, would it be the same? Why or why not? Provide your answer in a comment or print statement below.
  */
-
-
+var sameShip = falcon
+sameShip.position
+falcon.position
+sameShip.moveLeft()
+sameShip.position
+falcon.position
+// they are references to same object
+// if they were structs the position value would be different, because if they were structs the assignment to sameShip would create a new instance
 /*:
  
  _Copyright © 2017 Apple Inc._
