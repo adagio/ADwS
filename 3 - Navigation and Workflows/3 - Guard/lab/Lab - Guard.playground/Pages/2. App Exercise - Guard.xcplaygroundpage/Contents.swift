c5fc0ab4e824ecc548dd7afb5a620e8a7f4a11ed
Franco Cedillo <franco.cedillo@gmail.com>
2017-06-14 16:50:16 -0500

    Lab 3.3 Guard

--- 3 - Navigation and Workflows/3 - Guard/lab/Lab - Guard.playground/Pages/2. App Exercise - Guard.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/3 - Guard/lab/Lab - Guard.playground/Pages/2. App Exercise - Guard.xcplaygroundpage/Contents.swift
@@ -10,7 +10,18 @@ import UIKit
  
  Write a failable initializer that takes parameters for your start and end times, and then checks to see if they are greater than 10 seconds apart using a guard statement. If they are, your initializer should fail. Otherwise, the initializer should set the properties accordingly.
  */
-
+struct Workout{
+    let startTime: Double
+    let endTime: Double
+    
+    init?(startTime: Double, endTime: Double) {
+        guard ( endTime - startTime ) < 10000 else {
+            return nil }
+        
+        self.startTime = startTime
+        self.endTime = endTime
+    }
+}
 
 /*:
  Imagine a screen where a user inputs a meal that they've eaten. If the user taps a "save" button without adding any food, you might want to prompt the user that they haven't actually added anything.
@@ -26,14 +37,17 @@ let foodTextField = UITextField()
 let caloriesTextField = UITextField()
 
 foodTextField.text = "Banana"
-caloriesTextField.text = "23"
+caloriesTextField.text = "2y3"
 
-
+func logFood() -> Food? {
+    guard let fT = foodTextField.text, let cT = caloriesTextField.text, let c = Int(cT) else { return nil }
+    return Food(name: fT, calories: c)
+}
 /*:
  Call the function you made above and capture the return value. Unwrap the `Food` object with standard optional binding and print a statement about the food using each of its properties. Go back and change the text in `caloriesTextField` to a string that cannot be converted into a number. What happens in that case?
  */
-
-
+let f = logFood()
+// if caloriesTextField cannot be converted into a number the function returns nil
 /*:
  
  _Copyright © 2017 Apple Inc._
